7e1e9d7aed1306fdcc7ad8626cfaf8f42ba93754
Jerrypie <xieshengcheng@hust.edu.cn>
2018-12-17 19:43:55 +0800

    update

---
+++ demo/src/main/java/com/example/demo/controll/LoginControll.java
@@ -0,0 +1,19 @@
+package com.example.demo.controll;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginControll {
+    
+	@RequestMapping( {"","/","/index"} )
+	public String loginUp() {
+	    return "/register";
+    }
+	
+	@PostMapping("/login")
+	public String hello() {
+	    return "/hello";
+	}
+}
