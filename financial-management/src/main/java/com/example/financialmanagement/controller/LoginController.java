f04904c75f52630f9bdae0fec3d9c94592a98cd1
mudifan <difanmu@gmail.com>
2018-12-20 16:58:34 +0800

    update

--- financial-management/src/main/java/com/example/financialmanagement/controller/LoginController.java
+++ financial-management/src/main/java/com/example/financialmanagement/controller/LoginController.java
@@ -2,21 +2,12 @@ package com.example.financialmanagement.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-
-    @RequestMapping({"", "/", "/index"})
-    public String IndexPage() {
-        return "index.html";
-    }
-
+    //接收登录信息，return到MainController
     @PostMapping("/login")
     public String login(String UserId, String Password) {
-        if ("xsc".equals(UserId) && "caiji".equals(Password)) {
-            return "/main";
-        }
         return "/loginError";
     }
 }
