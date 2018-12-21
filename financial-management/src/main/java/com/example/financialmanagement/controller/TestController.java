c4326f16720c54120251d1d5a90bdf1ff5d3668d
Jerrypie <xieshengcheng@hust.edu.cn>
2018-12-21 17:00:45 +0800

    update

--- financial-management/src/main/java/com/example/financialmanagement/controller/TestController.java
+++ financial-management/src/main/java/com/example/financialmanagement/controller/TestController.java
@@ -1,52 +1,52 @@
-// package com.example.financialmanagement.controller;
+package com.example.financialmanagement.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import com.example.financialmanagement.model.BasicRecord;
-// import com.example.financialmanagement.model.BasicRecordRepository;
-// import com.example.financialmanagement.model.User;
-// import com.example.financialmanagement.model.UserRepository;
+import com.example.financialmanagement.model.BasicRecord;
+import com.example.financialmanagement.model.BasicRecordRepository;
+import com.example.financialmanagement.model.User;
+import com.example.financialmanagement.model.UserRepository;
 
-// import org.aspectj.lang.annotation.Before;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// //测试数据库用
-// public class TestController {
-//     @Autowired
-//     public BasicRecordRepository basicRecordRepository;
-//     @Autowired
-//     public UserRepository userRepository;
+@RestController
+//测试数据库用
+public class TestController {
+    @Autowired
+    public BasicRecordRepository basicRecordRepository;
+    @Autowired
+    public UserRepository userRepository;
 
-//     public void initdate(){
-//         basicRecordRepository.deleteAll();
-//         userRepository.deleteAll();
+    public void initdate(){
+        basicRecordRepository.deleteAll();
+        userRepository.deleteAll();
 
-//         BasicRecord basicRecord = new BasicRecord();
-//         basicRecord.setValue(10);
-//         basicRecord.setOther("测试1");
-//         basicRecordRepository.save(basicRecord);
-//         basicRecord.setValue(5);
-//         basicRecord.setOther("测试2");
-//         basicRecordRepository.save(basicRecord);
+        BasicRecord basicRecord = new BasicRecord();
+        basicRecord.setValue(10);
+        basicRecord.setOther("测试1");
+        basicRecordRepository.save(basicRecord);
+        basicRecord.setValue(5);
+        basicRecord.setOther("测试2");
+        basicRecordRepository.save(basicRecord);
 
-//         User user = new User();
-//         user.setUsername("longshen");
-//         user.setPassword("nb");
-//         userRepository.save(user);
+        User user = new User();
+        user.setUsername("longshen");
+        user.setPassword("nb");
+        userRepository.save(user);
 
-//         List<BasicRecord> records = basicRecordRepository.findAll();
-//         user.setRecords(records);
-//         userRepository.save(user);
-//     }
+        List<BasicRecord> records = basicRecordRepository.findAll();
+        user.setRecords(records);
+        userRepository.save(user);
+    }
 
-//     @RequestMapping("/test1")  
-//     public void getAllUsers(){
-//         User user = new User();
-//         user.setPassword("password");
-//         user.setUsername("username");
-//         user.setEmail("email");
-//     }  
-// }
+    @RequestMapping("/test1")  
+    public void getAllUsers(){
+        User user = new User();
+        user.setPassword("password");
+        user.setUsername("username");
+        user.setEmail("email");
+    }  
+}
