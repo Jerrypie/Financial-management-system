8c38f23f7b0087b6a2443530df79f08c4ee9b8f1
mudifan <difanmu@gmail.com>
2018-12-22 11:06:22 +0800

    add favico

--- financial-management/src/main/java/com/example/financialmanagement/controller/MainController.java
+++ financial-management/src/main/java/com/example/financialmanagement/controller/MainController.java
@@ -1,16 +1,19 @@
 package com.example.financialmanagement.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @RequestMapping("/main")
     public String hello() {
-        return "Hello World!";
+        return "main";
     }
 
+    @ResponseBody
     @RequestMapping("/loginError")
     public String loginError() {
         return "Password is wrong! Please login again!";
