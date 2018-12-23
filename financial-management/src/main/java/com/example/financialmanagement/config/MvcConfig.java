30d05eec195cac05233eacc5ec12fa175afd7453
mudifan <difanmu@gmail.com>
2018-12-23 15:58:51 +0800

    更换拦截器，解决不能访问静态资源问题

--- financial-management/src/main/java/com/example/financialmanagement/config/MvcConfig.java
+++ financial-management/src/main/java/com/example/financialmanagement/config/MvcConfig.java
@@ -2,15 +2,13 @@ package com.example.financialmanagement.config;
 
 import com.example.financialmanagement.component.LoginHandlerInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 
 //扩展MVC功能
 @Configuration
-public class MvcConfig extends WebMvcConfigurationSupport {
+public class MvcConfig implements WebMvcConfigurer {
     @Override
-    protected void addViewControllers(ViewControllerRegistry registry) {
+    public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
@@ -21,8 +19,9 @@ public class MvcConfig extends WebMvcConfigurationSupport {
 
     //注册拦截器，除了特定的请求都拦截
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns("/index.html","/","/index","/signup","/login.action","/*.action");
+        WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
