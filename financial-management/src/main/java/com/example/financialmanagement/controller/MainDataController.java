eae35026b4a4fac2ab6a3335f9c7179a67be1801
Jerrypie <xieshengcheng@hust.edu.cn>
2019-01-09 17:23:28 +0800

    update

--- financial-management/src/main/java/com/example/financialmanagement/controller/MainDataController.java
+++ financial-management/src/main/java/com/example/financialmanagement/controller/MainDataController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -32,12 +35,14 @@ public class MainDataController {
 
     private User user;
 
+    //删除某条记录
     @DeleteMapping(value = "/main/record")
     public boolean deleteRecord(@RequestParam("id") int recordId) {
         System.out.println(recordId);
         return recordService.deleteByRecordnum(recordId);
     }
 
+    //按类型返回记录
     @GetMapping("/main/record/type")
     public List<BasicRecord> getByCategory(@RequestParam(value = "category") int category, HttpServletRequest request) {
         // 从session 中取出User
@@ -50,6 +55,7 @@ public class MainDataController {
         return records;
     }
 
+    //按时间返回记录
     @GetMapping("/main/record/time")
     public List<BasicRecord> getByRecordtime(@RequestParam("inTime") int time, HttpServletRequest request) {
         //从session 中取出User
@@ -75,6 +81,40 @@ public class MainDataController {
         return records;
     }
 
+    //输入日期的String格式，输出Calendar格式的日期
+    private Calendar DateTransform(String Originrecordtime) throws Exception {
+        SimpleDateFormat StrParse = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = StrParse.parse(Originrecordtime);
+        Calendar recordtime = Calendar.getInstance();
+        recordtime.setTime(date);
+        return recordtime;
+    }
+
+    //按所给时间段返回数据
+    @GetMapping("/main/record/someTime")
+    public List<BasicRecord> getRecordsInTime(@RequestParam("timestart") String startrecordtime,
+                                            @RequestParam("timeend") String endrecordtime,
+                                            HttpServletRequest request) throws Exception {
+
+        Calendar timestart = DateTransform(startrecordtime);
+        Calendar timeend = DateTransform(startrecordtime);
+        timestart.add(Calendar.MINUTE,-1);
+        timeend.add(Calendar.MINUTE,1);
+
+        //取出用户的记录
+        HttpSession session = request.getSession();
+        user = (User) session.getAttribute("UserObj");
+        mainService.setUser(user);
+        List<BasicRecord> records = mainService.getAllSortedRecordsOfUser();
+        RecordService recordService = new RecordService();
+
+        records = recordService.recordsOfSomeDays(records,timestart,timeend);
+
+        return records;
+    }
+
+
+    //按收入或支出返回记录
     @GetMapping("/main/record/value")
     public List<BasicRecord> getByValue(@RequestParam(value = "income") int income, HttpServletRequest request) {
         // 从session 中取出User
@@ -85,6 +125,41 @@ public class MainDataController {
         List<BasicRecord> records = mainService.getAllValueRecord(income);
 
         return records;
+    }
+
+    @GetMapping("/main/record/totalValue")
+    public double[] getTotalValue( HttpServletRequest request) {
+        double[] totalvalue = new double[2];
+        //从session 中取出User
+        HttpSession session = request.getSession();
+        user = (User) session.getAttribute("UserObj");
+        mainService.setUser(user);
+        List<BasicRecord> income = mainService.getAllValueRecord(1);
+        List<BasicRecord> outcome = mainService.getAllValueRecord(0);
+        RecordService recordService = new RecordService();
+
+        int time = 3;
+        if(time == 1){
+            income = recordService.recordsOfThreeDays(income);
+            outcome = recordService.recordsOfThreeDays(outcome);
+        }
+        else if(time == 2 ){
+            income = recordService.recordsOfThisWeek(income);
+            outcome = recordService.recordsOfThisWeek(outcome);
+        }
+        else if(time == 3){
+            income = recordService.recordsOfThisMonth(income);
+            outcome = recordService.recordsOfThisMonth(outcome);
+        }
+        else if(time == 4){
+            income = recordService.recordsOfThisYear(income);
+            outcome = recordService.recordsOfThisYear(outcome);
+        }
+
+        totalvalue[0] = recordService.getTotalValueOfRecords(income);
+        totalvalue[1] = recordService.getTotalValueOfRecords(outcome);
+
+        return totalvalue;
     }
 
 }
--- financial-management/src/main/java/com/example/financialmanagement/controller/TestController.java
+++ financial-management/src/main/java/com/example/financialmanagement/controller/TestController.java
@@ -4,26 +4,17 @@ import java.util.Calendar;
 import java.util.List;
 
 import javax.annotation.Resource;
-import javax.persistence.Basic;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import com.example.financialmanagement.model.*;
+import com.example.financialmanagement.model.BasicRecord;
+import com.example.financialmanagement.model.BasicRecordRepository;
+import com.example.financialmanagement.model.User;
+import com.example.financialmanagement.model.UserRepository;
 import com.example.financialmanagement.service.MainService;
 import com.example.financialmanagement.service.RecordService;
 import com.example.financialmanagement.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
