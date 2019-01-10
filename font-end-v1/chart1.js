27b3e86f1f8a9148f93cb6d4778ac495549acb94
Phantasiebild <haruhi@hust.edu.cn>
2019-01-10 11:50:16 +0800

    updata

--- font-end-v1/chart1.js
+++ font-end-v1/chart1.js
@@ -57,7 +57,7 @@ function bindData(){
 
             myChart.setOption({
                 xAxis:{
-                    data:data.name
+                    data:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
                 },
                 series:[
                     {
